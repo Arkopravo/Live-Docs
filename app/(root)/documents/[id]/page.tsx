5024a02c488109670e2c83075e15af6c74b6d7eb
Arkopravo Ghosh <arkopravoghosh789@gmail.com>
2024-08-07 20:43:15 +0530

    auth clerk half

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,16 +1,31 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/clerk-react'
+import { SignedOut, SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
+
       <Header>
+        
         <div className='flex w-fit items-center justify-center gap-2'>
           <p className='document-title'>fake docs</p>
         </div>
+
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+
       </Header>
+
       <Editor/>
+
     </div>
   )
 }
